ed984d2c94abf8e57f6aa97a3463ca8923b3369a
Macko Martin <macko.mar@gmail.com>
2014-04-04 10:37:18 +0200

    chapter 3, exercise 1

---
+++ chapter3/ex3_1.lua
@@ -0,0 +1,16 @@
+--[[
+--  Chapter 3, Exercise 1
+--
+--	What will the following program print?
+--
+--	for i = -10, 10 do
+--		print(i, i % 3)
+--	end
+--
+--  Solution:
+--  It will print a number, and a modulo 3 of that number from -10 to 10
+-]]
+
+for i=-10,10 do
+	print(i, i % 3)
+end
