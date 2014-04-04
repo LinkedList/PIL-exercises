e0d638301de677c5037577047a82055934885afa
Macko Martin <macko.mar@gmail.com>
2014-04-04 09:12:51 +0200

    added header to chapter 2, exercise 4

--- chapter2/ex2_4.lua
+++ chapter2/ex2_4.lua
@@ -1,8 +1,18 @@
+--[=[
+--  Chapter 2, Exercise 4
+--
+--  How can you embed the following piece of XML as a string in Lua?
+--  <![CDATA[
+--    Hello world
+--  ]]>
+--
+--  Solution:
+-]=]
 local string = "<![CDATA[\n  Hello World\n]]"
 print(string)
 string = [===[
 <![CDATA[
-  Hello World		
+  Hello World
 ]]
 ]===]
 print(string)
