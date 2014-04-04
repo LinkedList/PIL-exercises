8b22a68c67ae24e0acc42fbc3a8f8d7504ee11d7
Macko Martin <macko.mar@gmail.com>
2014-04-04 14:48:46 +0200

    chapter 3, exercise 6

---
+++ chapter3/ex3_6.lua
@@ -0,0 +1,12 @@
+--[[
+--  Chapter 3, Exercise 6
+--
+--	Consider the following expression:
+--	(x and y and (not z)) or ((not y) and x)
+--	Are the parentheses necessary? Would you recommend their use in that expression?
+--
+--  Solution:
+--  Parentheses in this expression are not necessary, as "not" has precedence over "and", and "and" has precedence over "or".
+--  However usage of parentheses is advised, as it promotes readability.
+-]]
+
