5553bad8186c95899ca8f90ccff1d42d36160518
linkedlist <macko.mar@gmail.com>
2014-04-05 13:04:30 +0200

    chapter 4, exercise 1

---
+++ chapter4/ex4_1.lua
@@ -0,0 +1,11 @@
+--[[
+--  Chapter 4, Exercise 1
+--
+--	Most languages with a C-like syntax do not offer an elseif construct.
+--	Why does Lua need this construct more than those languages?
+--
+--  Solution:
+--  Because Lua doesn't have a switch statement.
+-]]
+
+
