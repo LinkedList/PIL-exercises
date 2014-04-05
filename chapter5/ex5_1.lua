32d45594559b009d5429841725ddb9dd4bdbff04
linkedlist <macko.mar@gmail.com>
2014-04-05 20:19:53 +0200

    chapter 5, exercise 1

---
+++ chapter5/ex5_1.lua
@@ -0,0 +1,18 @@
+--[[
+--  Chapter 5, Exercise 1
+--
+--	Write a function that receives an arbitrary number of strings and returns 
+--	all of them concatenated together.
+--
+--  Solution:
+--]]
+
+function concatenatedStrings(...)
+	local result = ""
+	for str in pairs{...} do
+		result = result .. str
+	end
+	return result
+end
+
+print(concatenatedStrings("a", "b", "c", "d", "e"))
