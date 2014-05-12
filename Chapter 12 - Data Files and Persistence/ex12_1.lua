d06935323575e0b4ee2001b17a214697b751c666
Macko Martin <macko.mar@gmail.com>
2014-05-12 14:31:24 +0200

    chapter 12, exercise 1

---
+++ Chapter 12 - Data Files and Persistence/ex12_1.lua
@@ -0,0 +1,47 @@
+--[[
+--  Chapter 12, Exercise 1
+--
+--  Modify the code in Listing 12.2 so that it indents nested tables.
+--  (Hint: add an extra parameter to serialize with the indentation string.)
+--
+--  Solution:
+--]]
+
+function serialize(o, tab)
+    local tab = tab or 0
+    if type(o)  == "number" then
+        io.write(o)
+    elseif type(o) == "string" then
+        io.write(string.format("%q", o))
+    elseif type(o) == "table" then
+        io.write("{\n")
+        for k, v in pairs(o) do
+            for i=1, tab do
+                io.write("\t")
+            end
+            io.write(" ", k, " = ")
+            serialize(v, tab + 1)
+            io.write(", \n")
+        end
+            for i=1, tab do
+                io.write("\t")
+            end
+        io.write("}\n")
+    else
+        error("cannot serialize a " .. type(o))
+    end
+end
+
+local table = {
+    a = 1,
+    b = 2,
+    c = {
+        d = "string",
+        e = "another string",
+        f = {
+            1, 2, 3, 4
+        }
+    }
+}
+
+serialize(table)
