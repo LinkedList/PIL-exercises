529c9aa67dbfb9907e2c84bf75bc1b5e7a32ccd8
Macko Martin <macko.mar@gmail.com>
2014-05-13 09:02:48 +0200

    typo

--- Chapter 12 - Data Files and Persistence/ex12_3.lua
+++ Chapter 12 - Data Files and Persistence/ex12_3.lua
@@ -1,5 +1,5 @@
 --[[
---  Chapter 12, Exercise 1
+--  Chapter 12, Exercise 3
 --
 --  Modify the code of the previous exercise so that is uses the syntax
 --  ["key"]=value only when necessary (that is, when the key is a string but
