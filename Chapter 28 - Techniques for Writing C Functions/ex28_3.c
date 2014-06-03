9e623e5d995241a44f9f8606d1182ca16f4833b9
Martin Macko <macko.mar@gmail.com>
2014-06-03 20:50:15 +0200

    chapter 28, exercise 3, finished

--- Chapter 28 - Techniques for Writing C Functions/ex28_3.c
+++ Chapter 28 - Techniques for Writing C Functions/ex28_3.c
@@ -12,39 +12,6 @@
 #include "lua5.2/lauxlib.h"
 #include "lua5.2/lualib.h"
 
-static void stackDump(lua_State *L) {
-	int i;
-	int top = lua_gettop(L);
-
-	for(i = 1; i <= top; i++) {
-		int t = lua_type(L, i);
-
-		switch(t){
-			case LUA_TSTRING: {
-				printf("'%s'", lua_tostring(L, i));
-				break;
-			}
-
-			case LUA_TBOOLEAN: {
-				printf(lua_toboolean(L, i) ? "true" : "false");
-				break;
-			}
-
-			case LUA_TNUMBER: {
-				printf("'%g'", lua_tonumber(L, i));
-				break;
-			}
-
-			default: {
-				printf("'%s'", lua_typename(L, t));
-				break;
-			}
-		}
-		printf(" ");
-	}
-
-	printf("\n");
-}
 int static l_transliterate(lua_State *L) {
 	const char *s = luaL_checkstring(L, 1);
 	luaL_checktype(L, 2, LUA_TTABLE);
@@ -54,26 +21,33 @@ int static l_transliterate(lua_State *L) {
 	int len = lua_tonumber(L, -1);
 	lua_pop(L, 1);
 
+	// string buffer
 	luaL_Buffer b;
 	luaL_buffinitsize(L, &b, len);
 
 	int i;
 	for(i = 0; i < len; i++) {
+		// get char from original
 		char c[] = "x";
 		c[0] = s[i];
+		// getfield of key s[i]
 		lua_getfield(L, 2, c);
 
+		// test if nil
 		int nil = lua_isnil(L, -1);
 		if(nil){
+			// if nil don't change anything
 			luaL_addchar(&b, s[i]);
 		} else {
-			const char *c = lua_tolstring(L, -1, 1);
-			fprintf(stdout, "%s", c);
-			luaL_addchar(&b, c);
+			// if not add value from transliterate table
+			const char *c = lua_tostring(L, -1);
+			luaL_addstring(&b, c);
 		}
+		// remove pushed field
 		lua_pop(L, 1);
 	}
 
+	//return built string
 	luaL_pushresult(&b);
 	return 1;
 }
